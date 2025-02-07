83771ceea336ccdcc3e7c6ce11318f1ed1c8cc3d
eyob <eyob@gmail.com>
2025-02-07 16:21:58 +0300

    finale

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,10 +5,12 @@ import Footer from "./Footer";
 export default function Todo(){
     const [todos,settodos]=useState([])
     const s=todos.filter(todo=>todo.read).length
+    
+    var sss=todos.slice().sort((a,b)=>Number(a.read)-Number(b.read));
     return (
         <div className="header">
             <Form todos={todos} settodos={settodos}/>
-            <State todos={todos} settodos={settodos}/>
+            <State todos={sss} settodos={settodos}/>
             <Footer done={s} total={todos.length}/>
         </div>
             
